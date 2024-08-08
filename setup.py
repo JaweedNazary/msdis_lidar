b613a248d20a5ab745886ba1e0b833d9a12338bd
Jaweed Nazary <69158778+JaweedNazary@users.noreply.github.com>
2024-08-08 16:52:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Automatic_LiDAR_Download_from_MSDIS',
+    name='msdis_lidar',
     version='0.1',
     packages=find_packages(),
     install_requires=[
